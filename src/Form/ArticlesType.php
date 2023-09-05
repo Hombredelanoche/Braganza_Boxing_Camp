02e7d23539393a40ca4112e950225514847badb7
Elhombredelanoche <elbrandonpro@yahoo.fr>
2023-09-05 17:46:08 +0200

    Création des différents formulaires

---
+++ src/Form/ArticlesType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticlesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('content')
+            ->add('liked')
+            ->add('unliked')
+            ->add('createdAt')
+            ->add('updatedAt')
+            ->add('user')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
---
+++ tests/Controller/MessageControllerTest.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Test\Controller;
+
+use App\Entity\Message;
+use App\Repository\MessageRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class MessageControllerTest extends WebTestCase
+{
+    private KernelBrowser $client;
+    private MessageRepository $repository;
+    private string $path = '/message/';
+    private EntityManagerInterface $manager;
+
+    protected function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->repository = static::getContainer()->get('doctrine')->getRepository(Message::class);
+
+        foreach ($this->repository->findAll() as $object) {
+            $this->manager->remove($object);
+        }
+    }
+
+    public function testIndex(): void
+    {
+        $crawler = $this->client->request('GET', $this->path);
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Message index');
+
+        // Use the $crawler to perform additional assertions e.g.
+        // self::assertSame('Some text on the page', $crawler->filter('.p')->first());
+    }
+
+    public function testNew(): void
+    {
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $this->markTestIncomplete();
+        $this->client->request('GET', sprintf('%snew', $this->path));
+
+        self::assertResponseStatusCodeSame(200);
+
+        $this->client->submitForm('Save', [
+            'message[title]' => 'Testing',
+            'message[Content]' => 'Testing',
+            'message[createdAt]' => 'Testing',
+            'message[updatedAt]' => 'Testing',
+            'message[user]' => 'Testing',
+        ]);
+
+        self::assertResponseRedirects('/message/');
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+    }
+
+    public function testShow(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Message();
+        $fixture->setTitle('My Title');
+        $fixture->setContent('My Title');
+        $fixture->setCreatedAt('My Title');
+        $fixture->setUpdatedAt('My Title');
+        $fixture->setUser('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+
+        self::assertResponseStatusCodeSame(200);
+        self::assertPageTitleContains('Message');
+
+        // Use assertions to check that the properties are properly displayed.
+    }
+
+    public function testEdit(): void
+    {
+        $this->markTestIncomplete();
+        $fixture = new Message();
+        $fixture->setTitle('My Title');
+        $fixture->setContent('My Title');
+        $fixture->setCreatedAt('My Title');
+        $fixture->setUpdatedAt('My Title');
+        $fixture->setUser('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        $this->client->request('GET', sprintf('%s%s/edit', $this->path, $fixture->getId()));
+
+        $this->client->submitForm('Update', [
+            'message[title]' => 'Something New',
+            'message[Content]' => 'Something New',
+            'message[createdAt]' => 'Something New',
+            'message[updatedAt]' => 'Something New',
+            'message[user]' => 'Something New',
+        ]);
+
+        self::assertResponseRedirects('/message/');
+
+        $fixture = $this->repository->findAll();
+
+        self::assertSame('Something New', $fixture[0]->getTitle());
+        self::assertSame('Something New', $fixture[0]->getContent());
+        self::assertSame('Something New', $fixture[0]->getCreatedAt());
+        self::assertSame('Something New', $fixture[0]->getUpdatedAt());
+        self::assertSame('Something New', $fixture[0]->getUser());
+    }
+
+    public function testRemove(): void
+    {
+        $this->markTestIncomplete();
+
+        $originalNumObjectsInRepository = count($this->repository->findAll());
+
+        $fixture = new Message();
+        $fixture->setTitle('My Title');
+        $fixture->setContent('My Title');
+        $fixture->setCreatedAt('My Title');
+        $fixture->setUpdatedAt('My Title');
+        $fixture->setUser('My Title');
+
+        $this->manager->persist($fixture);
+        $this->manager->flush();
+
+        self::assertSame($originalNumObjectsInRepository + 1, count($this->repository->findAll()));
+
+        $this->client->request('GET', sprintf('%s%s', $this->path, $fixture->getId()));
+        $this->client->submitForm('Delete');
+
+        self::assertSame($originalNumObjectsInRepository, count($this->repository->findAll()));
+        self::assertResponseRedirects('/message/');
+    }
+}
